e36482c451059bf4b4403ff0a328cc2fdd3912f4
muhammadzidanfadillah <115553474+muhammadzidanfadilah@users.noreply.github.com>
2024-01-07 04:56:20 -0800

    Add files via upload

---
+++ Penjualan-kue-master/admin/footer.php
@@ -0,0 +1,5 @@
+<div class="copy" style="background-color: hsl(234, 100%, 76%); padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; Muhammad Zidan Fadillah</span>
+</footer>
+</body>
+</html>
---
+++ Penjualan-kue-master/footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid hsl(234, 100%, 76%);  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: hsl(234, 100%, 76%);"><b>ZIDAN KUE</b></h3>
+					<p>Jl. garuda persada 9 No.10C</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6287804616097</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> ZIDAN-KUE@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: hsl(234, 100%, 76%); padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; Muhammad Zidan Fadillah</span>
+		</div>
+	</footer>
+
+</body>
+</html>
